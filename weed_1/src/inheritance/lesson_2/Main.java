1627b678fcb7e784556fee5525723903409d2c64
buivanlong2002 <longbui20021008@gmail.com>
2025-04-14 18:54:17 +0700

    addfile

---
+++ weed_1/src/inheritance/lesson_2/Main.java
@@ -0,0 +1,18 @@
+package inheritance.lesson_2;
+
+public class Main {
+    public static void main(String[] args) {
+        Point2D point2D = new Point2D();
+        point2D.setXY(2.5F, 1.5F);
+        System.out.println(point2D);
+        for (float value : point2D.getXY()) {
+            System.out.println(value);
+        }
+
+        Point3D point3D = new Point3D(2.5F, 1.5F,5.5F);
+        System.out.println(point3D);
+        for (float value : point3D.getXY()) {
+            System.out.println(value);
+        }
+    }
+}
