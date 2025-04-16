b49cca089512bd60188f4344a57a5744dd89058f
buivanlong2002 <longbui20021008@gmail.com>
2025-04-16 09:01:56 +0700

    addfile

--- weed_1/src/access_modifier/th_2/Main.java
+++ weed_1/src/access_modifier/th_2/Main.java
@@ -1,4 +1,10 @@
 package access_modifier.th_2;
 
 public class Main {
+    public static void main(String[] args) {
+        Car car1 = new Car("Mazda 3","Skyactiv 3");
+        System.out.println(Car.numberOfCars);
+        Car car2 = new Car("Mazda 6","Skyactiv 6");
+        System.out.println(Car.numberOfCars);
+    }
 }
