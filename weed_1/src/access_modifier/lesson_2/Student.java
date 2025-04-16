2a1a7b13cf317e5788a397b0602bfcbb45121c19
buivanlong2002 <longbui20021008@gmail.com>
2025-04-16 09:10:40 +0700

    addfile

---
+++ weed_1/src/access_modifier/lesson_2/Student.java
@@ -0,0 +1,21 @@
+package access_modifier.lesson_2;
+
+public class Student {
+    private String name = "John";
+    private String classes = "C02";
+
+    public Student() {
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setClasses(String classes) {
+        this.classes = classes;
+    }
+
+    public String getInfo() {
+        return "Name: " + name + ", Class: " + classes;
+    }
+}
